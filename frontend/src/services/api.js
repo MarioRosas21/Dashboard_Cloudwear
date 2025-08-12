db4529c283ff67dcc7a9f48240ad13aa6e6ee346
LENOVO <LENOVO@DESKTOP-IN5HT21>
2025-08-12 13:20:51 -0600

    Ruta del back modificada

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.1.128:5000/api", // Cambia si despliegas
+  baseURL: "https://dashboard-cloudwear-1.onrender.com/api", // Cambia si despliegas
   //baseURL: "http://localhost:5000/api"
 });
 
