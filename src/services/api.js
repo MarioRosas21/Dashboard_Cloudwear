9cf3c1959d8e1b7c29f7d444029033cccddf6106
LENOVO <LENOVO@DESKTOP-IN5HT21>
2025-08-12 13:08:55 -0600

    Estructura separada

--- src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  //baseURL: "http://192.168.100.82:5000/api", // Cambia si despliegas
-  baseURL: "http://localhost:5000/api"
-});
-
-export default api;
